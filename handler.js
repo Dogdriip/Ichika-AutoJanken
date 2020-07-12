ff089b7deae52d0dbdd7d2ad2b3228051323c106
Hyunseung Jeon <dogdriip@gmail.com>
2020-07-13 01:22:09 +0900

    Fix #2 - Add pptr to dependency
    Cannot use `puppeteer` due to lambda's size limit.
    Use `puppeteer-core` and `chrome-aws-lambda` instead.

--- handler.js
+++ handler.js
@@ -1,6 +1,32 @@
 'use strict';
 
+const chromium = require("chrome-aws-lambda");
+
+const BASE_URL = "https://p.eagate.573.jp/"
+const LOGIN_URL = BASE_URL + "gate/p/login.html";
+const LOGIN_AUTH_URL = BASE_URL + "gate/p/common/login/api/login_auth.html";
+const LOGIN_RESRV_URL = BASE_URL + "gate/p/login_complete.html";
+const CAPTCHA_URL = BASE_URL + "gate/p/common/login/api/kcaptcha_generate.html";
+const JANKEN_URL = BASE_URL + "game/bemani/bjm2020/janken/index.html";
+
 module.exports.hello = async event => {
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    // headless: chromium.headless,
+    headless: true,  // default is also true
+  });
+  const page = await browser.newPage();
+  await page.goto(LOGIN_URL);
+  
+  const test = await page.$("#id_lgfm_inline > div > div.cl_lgfm_eavd.cl_lgfm_title");
+  const text = await page.evaluate(e => e.textContent, test);
+
+  console.log(text);
+
+  await browser.close();
+
   return {
     statusCode: 200,
     body: JSON.stringify(
---
+++ testbench.js
@@ -0,0 +1,27 @@
+const chromium = require("chrome-aws-lambda");
+
+try {
+  (async() => {
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      // headless: chromium.headless,
+      headless: true,  // default is also true
+    });
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+    
+    const body = await page.$("body");
+    const text = await page.evaluate(e => e.textContent, body);
+    
+    console.log(text);
+    
+    await browser.close();
+  })();
+  
+
+
+} catch (e) {
+  console.error(e);
+}
