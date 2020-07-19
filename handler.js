9d27b50f50589259779f1399049f4d2fa9a79787
Hyunseung Jeon <dogdriip@gmail.com>
2020-07-19 11:38:29 +0900

    Fix #8 - Configure ESLint (w/ airbnb base)

--- handler.js
+++ handler.js
@@ -1,46 +1,40 @@
-'use strict';
+const chromium = require('chrome-aws-lambda');
 
-const chromium = require("chrome-aws-lambda");
+const login = require('./login');
 
-const login = require("./login");
-
-const BASE_URL = "https://p.eagate.573.jp/"
-const LOGIN_URL = BASE_URL + "gate/p/login.html";
-const LOGIN_AUTH_URL = BASE_URL + "gate/p/common/login/api/login_auth.html";
-const LOGIN_RESRV_URL = BASE_URL + "gate/p/login_complete.html";
-const CAPTCHA_URL = BASE_URL + "gate/p/common/login/api/kcaptcha_generate.html";
-const JANKEN_URL = BASE_URL + "game/bemani/bjm2020/janken/index.html";
+// const BASE_URL = 'https://p.eagate.573.jp/';
+// const LOGIN_URL = `${BASE_URL}gate/p/login.html`;
+// const LOGIN_AUTH_URL = `${BASE_URL}gate/p/common/login/api/login_auth.html`;
+// const LOGIN_RESRV_URL = `${BASE_URL}gate/p/login_complete.html`;
+// const CAPTCHA_URL = `${BASE_URL}gate/p/common/login/api/kcaptcha_generate.html`;
+// const JANKEN_URL = `${BASE_URL}game/bemani/bjm2020/janken/index.html`;
 
 module.exports.hello = async (event) => {
-    const browser = await chromium.puppeteer.launch({
-        args: chromium.args,
-        defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath,
-        // headless: chromium.headless,  // will launch headless chrome
-        headless: false,  // default is also true
-    });
-    
-    const res = await login.eagate_login(browser);
-    console.log(res);
-    
-    
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+    // headless: chromium.headless,  // will launch headless chrome
+    headless: false, // default is also true
+  });
 
-    
-    await browser.close();
-    
-    return {
-        statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: 'Go Serverless v1.0! Your function executed successfully!',
-                input: event,
-            },
-            null,
-            2
-            ),
-    };
-    
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  const res = await login.eagateLogin(browser);
+  console.log(res);
+
+  await browser.close();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2,
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
-    
--- login.js
+++ login.js
@@ -1,14 +1,15 @@
-const chromium = require("chrome-aws-lambda");
-const fs = require("fs");
-const crypto = require("crypto");
+// eslint-disable-next-line no-unused-vars
+const chromium = require('chrome-aws-lambda');
+const fs = require('fs');
+const crypto = require('crypto');
 
-const BASE_URL = "https://p.eagate.573.jp/"
-const LOGIN_URL = BASE_URL + "gate/p/login.html";
-const LOGIN_AUTH_URL = BASE_URL + "gate/p/common/login/api/login_auth.html";
-const LOGIN_RESRV_URL = BASE_URL + "gate/p/login_complete.html";
+const BASE_URL = 'https://p.eagate.573.jp/';
+const LOGIN_URL = `${BASE_URL}gate/p/login.html`;
+// const LOGIN_AUTH_URL = `${BASE_URL}gate/p/common/login/api/login_auth.html`;
+// const LOGIN_RESRV_URL = `${BASE_URL}gate/p/login_complete.html`;
 
-const CAPTCHA_JSON = "captcha.json";
-const LOGIN_DATA_JSON = "login_data.json";
+const CAPTCHA_JSON = 'captcha.json';
+const LOGIN_DATA_JSON = 'login_data.json';
 
 const rawdata = fs.readFileSync(CAPTCHA_JSON);
 const captchaAns = JSON.parse(rawdata);
@@ -16,79 +17,74 @@ const captchaAns = JSON.parse(rawdata);
 const data = fs.readFileSync(LOGIN_DATA_JSON);
 const loginData = JSON.parse(data);
 
-
-
 /**
  * Get image md5 hash from image URL. HTTP protocol URL.
  */
 const getImageHash = async (browser, url) => {
-    const page = await browser.newPage();
+  const page = await browser.newPage();
 
-    const imgView = await page.goto(url);
-    const buffer = await imgView.buffer();
-    
-    // Convert buffer to md5 hash.
-    let md5 = crypto.createHash('md5').update(buffer).digest('hex');
+  const imgView = await page.goto(url);
+  const buffer = await imgView.buffer();
 
-    await page.close();
-    return md5;
-}
+  // Convert buffer to md5 hash.
+  const md5 = crypto.createHash('md5').update(buffer).digest('hex');
 
+  await page.close();
+  return md5;
+};
 
 /**
  * Login to eagate.
  */
-const eagate_login = async (browser) => {
-    const page = await browser.newPage();
-    // Go to login page.
-    // await page.goto(LOGIN_URL);
-    await page.goto(LOGIN_URL, { waitUntil: 'networkidle2' });  // well...
+const eagateLogin = async (browser) => {
+  const page = await browser.newPage();
+  // Go to login page.
+  // await page.goto(LOGIN_URL);
+  await page.goto(LOGIN_URL, { waitUntil: 'networkidle2' }); // well...
 
-    // Get correct_pic.
-    // const corrPic = await page.$("#id_cpfm_correct_pic");
-    // const corrPicSrc = await page.evaluate(e => e.src, corrPic);
-    const corrPicSrc = await page.$eval("#id_cpfm_correct_pic", e => e.src);
-    console.log(`correct_pic : ${corrPicSrc}`);
+  // Get correct_pic.
+  // const corrPic = await page.$("#id_cpfm_correct_pic");
+  // const corrPicSrc = await page.evaluate(e => e.src, corrPic);
+  const corrPicSrc = await page.$eval('#id_cpfm_correct_pic', (e) => e.src);
+  console.log(`correct_pic : ${corrPicSrc}`);
 
-    // Get correct_pic md5 hash.
-    const corrPicHash = await getImageHash(browser, corrPicSrc);
-    console.log(`correct_pic md5 : ${corrPicHash}`);
+  // Get correct_pic md5 hash.
+  const corrPicHash = await getImageHash(browser, corrPicSrc);
+  console.log(`correct_pic md5 : ${corrPicHash}`);
 
+  // Get choice answers (5 hashes) from captchaAns - JSON parsed.
+  const choiceAns = captchaAns[corrPicHash].choice_md5;
+  console.log(choiceAns);
 
-    // Get choice answers (5 hashes) from captchaAns - JSON parsed.
-    const choiceAns = captchaAns[corrPicHash]['choice_md5'];
-    console.log(choiceAns);
+  // Iterate div > label, check each label's inner img hash, check if it's correct.
+  const choiceList = await page.$$('.cl_cpfm_choicelistbox > label');
+  for (const choice of choiceList) {
+    const choicePicSrc = await choice.$eval('img', (e) => e.src);
+    const choiceInput = await choice.$('input');
+    console.log(choicePicSrc);
 
-    // Iterate div > label, check each label's inner img hash, check if it's correct.
-    const choiceList = await page.$$(".cl_cpfm_choicelistbox > label");
-    for (let choice of choiceList) {
-        const choicePicSrc = await choice.$eval("img", e => e.src);
-        const choiceInput = await choice.$("input");
-        console.log(choicePicSrc);
+    const choicePicHash = await getImageHash(browser, choicePicSrc);
+    console.log(choicePicHash);
 
-        const choicePicHash = await getImageHash(browser, choicePicSrc);
-        console.log(choicePicHash);
-        
-        if (choiceAns.includes(choicePicHash)) {
-            await choiceInput.evaluate(e => e.checked = true);
-            console.log(`checked`);
-        }
-        
+    if (choiceAns.includes(choicePicHash)) {
+      await choiceInput.evaluate((e) => e.checked = true);
+      console.log('checked');
     }
+  }
 
-    // Fill input form with login_data.json.
-    // Be aware of usage of local variables in eval.
-    // See https://stackoverflow.com/questions/47966510/how-to-fill-an-input-field-using-puppeteer
-    await page.$eval(`input[name="nm_login_id"]`, (e, value) => e.value = value, loginData["id"]);
-    await page.$eval(`input[name="nm_paswords"]`, (e, value) => e.value = value, loginData["password"]);
+  // Fill input form with login_data.json.
+  // Be aware of usage of local variables in eval.
+  // See https://stackoverflow.com/questions/47966510/how-to-fill-an-input-field-using-puppeteer
+  await page.$eval('input[name="nm_login_id"]', (e, value) => e.value = value, loginData.id);
+  await page.$eval('input[name="nm_paswords"]', (e, value) => e.value = value, loginData.password);
 
-    // Click login btn and wait until loaded.
-    await page.click(".cl_lgfm_loginbtn");
-    await page.waitForNavigation();
+  // Click login btn and wait until loaded.
+  await page.click('.cl_lgfm_loginbtn');
+  await page.waitForNavigation();
 
-    // await page.screenshot({ path: 'test.png' });
+  // await page.screenshot({ path: 'test.png' });
 
-    return 'success';
-}
+  return 'success';
+};
 
-module.exports.eagate_login = eagate_login
+module.exports.eagateLogin = eagateLogin;
